3fe72a410dec671507a6853b9c8cf18f4048fb4a
Akashgreninja <akashuhulekal@gmail.com>
2023-08-13 21:30:36 +0530

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+
+const asciiArtFilePath = path.join(__dirname, './check.txt');
+const asciiArt = fs.readFileSync(asciiArtFilePath, 'utf-8');
+// ANSI escape codes for color formatting
+const colors = {
+    reset: '\x1b[0m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    cyan: '\x1b[36m',
+    magenta: '\x1b[35m',
+    bold: '\x1b[1m'
+  };
+  
+  console.log(colors.green + "Hello from Akash Uday Hulekal !" + colors.reset);
+  console.log(colors.green + "I am a developer from India " + colors.reset);
+  console.log(colors.green + "My hobbies include jogging, talking about cricket, Marvel, and DC." + colors.reset);
+  
+  console.log(colors.yellow + "\nHere are some more details about me:" + colors.reset);
+  
+  console.log(colors.cyan + "- 🔭 I’m currently working on DSA in Python" + colors.reset);
+  console.log(colors.cyan + "- 📫 I have a reputation of 461 on Stack Overflow and am in the top 6% of contributors worldwide in 2023." + colors.reset);
+  console.log(colors.cyan + "- 📫 I am ranked 526,726 on LeetCode and striving to improve." + colors.reset);
+  console.log(colors.cyan + "- 🌱 I’m currently learning concepts of DSA and Web3." + colors.reset);
+  console.log(colors.cyan + "- 👯 I’m looking to collaborate on Web3 projects." + colors.reset);
+  console.log(colors.cyan + "- 👯 I’m aiming to enhance my skills in hackathons and problem solving." + colors.reset);
+  console.log(colors.cyan + "- 💬 Ask me anything; I'm here to help." + colors.reset);
+  console.log(colors.cyan + "- 📫 How to reach me: akashuhulekal@gmail.com" + colors.reset);
+  
+  // ASCII art
+  console.log(colors.magenta + colors.bold + "\n\n,;;;;;;,            ;` ;;;;;;;;;;;;;;;;;;;;  ;;        ;;");
+  console.log(asciiArt);
---
+++ readme.md
@@ -0,0 +1,26 @@
+# AkashGreninja
+
+![GitHub](https://img.shields.io/github/license/your-username/akashgreninja)
+![GitHub last commit](https://img.shields.io/github/last-commit/your-username/akashgreninja)
+![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/your-username/akashgreninja)
+
+An npm package to display an animated ASCII art of "AkashGreninja" in the terminal.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Description
+
+AkashGreninja is an npm package that lets you display an animated ASCII art of "AkashGreninja" in the terminal using the `npx hello-akash` command. It's a fun way to showcase an animated ASCII art whenever you need a bit of terminal flair.
+
+## Installation
+
+To use the AkashGreninja package, you can run the following command in your terminal:
+
+```bash
+npx hello-akash
