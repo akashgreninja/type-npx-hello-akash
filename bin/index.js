9250235a5dc1a4831a7d43ab194429b98c8a7b2d
Akash Uday HULEKAL <a.hulekal@f5.com>
2024-09-20 10:47:01 +0530

    1.0.15

--- bin/index.js
+++ bin/index.js
@@ -16,16 +16,18 @@ const colors = {
   };
   
   console.log(colors.green + "Hello from Akash Uday Hulekal !" + colors.reset);
+  
   console.log(colors.green + "I am a developer from India " + colors.reset);
+   console.log(colors.green + "I work at F5 Networks" + colors.reset);
   console.log(colors.green + "My hobbies include jogging, talking about cricket, Marvel, and DC." + colors.reset);
   
   console.log(colors.yellow + "\nHere are some more details about me:" + colors.reset);
   
-  console.log(colors.cyan + "- 🔭 I’m currently working on DSA in Python" + colors.reset);
-  console.log(colors.cyan + "- 📫 I have a reputation of 461 on Stack Overflow and am in the top 6% of contributors worldwide in 2023." + colors.reset);
-  console.log(colors.cyan + "- 📫 I am ranked 526,726 on LeetCode and striving to improve." + colors.reset);
-  console.log(colors.cyan + "- 🌱 I’m currently learning concepts of DSA and Web3." + colors.reset);
-  console.log(colors.cyan + "- 👯 I’m looking to collaborate on Web3 projects." + colors.reset);
+  console.log(colors.cyan + "- 🔭 I’m currently working on Golang " + colors.reset);
+  console.log(colors.cyan + "- 📫 I have a reputation of 611 on Stack Overflow and am in the top 6% of contributors worldwide in 2023." + colors.reset);
+  console.log(colors.cyan + "- 📫 I am ranked 200,000 on LeetCode and striving to improve." + colors.reset);
+  console.log(colors.cyan + "- 🌱 I’m currently learning concepts of Networking" + colors.reset);
+  console.log(colors.cyan + "- 👯 I’m looking to collaborate on projects involving core concepts whichever field it might be" + colors.reset);
   console.log(colors.cyan + "- 👯 I’m aiming to enhance my skills in hackathons and problem solving." + colors.reset);
   console.log(colors.cyan + "- 💬 Ask me anything; I'm here to help." + colors.reset);
   console.log(colors.cyan + "- 📫 How to reach me: akashuhulekal@gmail.com" + colors.reset);
